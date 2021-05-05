db4eaf8e65f12474275ef6df7cfc9bc5e54d2638
juanf <jfruizg@unbosque.edu.co>
2021-05-04 21:55:49 -0500

    version4

---
+++ src/main/java/com/baeldung/persistencia/UsuarioDAO.java
@@ -0,0 +1,67 @@
+package com.baeldung.persistencia;
+
+import com.baeldung.bean.Usuario;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class UsuarioDAO {
+
+
+    private ObjectInputStream entrada;
+    private ObjectOutputStream salida;
+
+    public UsuarioDAO(File archivo) {
+
+        if (archivo.exists()) {
+            System.out.println("Existe");
+        } else {
+            try {
+                archivo.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public UsuarioDAO() {
+    }
+
+
+    public ArrayList<Usuario> leerArchivoJuegos(File archivo) {
+        ArrayList<Usuario> juegos = new ArrayList<Usuario>();
+        if (archivo.length() != 0) {
+            try {
+                entrada = new ObjectInputStream(new FileInputStream(archivo));
+                juegos = (ArrayList<Usuario>) entrada.readObject();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return juegos;
+    }
+
+    public void escribirEnArchivoJuegos(ArrayList<Usuario> juegos, File archivo) {
+        try {
+            salida = new ObjectOutputStream(new FileOutputStream(archivo));
+            salida.writeObject(juegos);
+            salida.close();
+        } catch (FileNotFoundException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/baeldung/servlets/GetUsuario.java
@@ -0,0 +1,44 @@
+package com.baeldung.servlets;
+
+
+import com.baeldung.bean.Usuario;
+import com.baeldung.persistencia.UsuarioDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+
+@WebServlet(name = "GetCities", urlPatterns = {"/GetCities"})
+public class GetUsuario extends HttpServlet {
+
+    UsuarioDAO us = new UsuarioDAO();
+    File file = new File("/Data/archivo.dat");
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+
+            List<Usuario> cities = us.leerArchivoJuegos(file);
+
+            Gson gson = new GsonBuilder()
+                    .excludeFieldsWithoutExposeAnnotation()
+                    .create();
+
+            out.print(gson.toJson(cities));
+        }
+    }
+}
